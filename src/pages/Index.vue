8865cfd47882df49b88c9e678670c45bb0fcd1e8
zigelzi <miika.a.savela@gmail.com>
2020-08-24 21:25:01 +0300

    Add main content to landing page and style it.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,80 @@
 <template>
   <Layout>
-    <div>
-      <h1>Viiru</h1>
-      <h2>Digital</h2>
+    <section id="hero-section">
+      <div class="hero-container">
+        <header>
+          <div class="logo-container">
+            <h1 class="logo-text-l logo-text" >Viiru</h1>
+            <h1 class="logo-text-m logo-text">Digital</h1>
+          </div>
+        </header>
+        <div class="hero-content">
+          <h2 class="hero-text-s hero-text"><span class="text-normal ">Pelaaminen</span> on harrastukseni</h2>
+          <h1 class="hero-text-l hero-text"><span class="text-normal text-pink" >Digitaaliset palvelut</span> intohimoni</h1>
+        </div>
+        
+      </div>
+    </section>
+    <div class="body-content">
+       <section id="introduction">
+          <div>
+            <h1 class="text-l">Digitaalisten palveluiden moniosaaja</h1>
+            <p>Moi!</p>
+            <p>Olen Miika Savela, digitaalisten tuotteiden ja tekniikan kanssa peuhaamista rakastava kaveri Lohjalta.</p>
+            <p><span class="text-pink">Viiru Digital</span> on yksi harrasteprojekteistani, jonka puitteissa kehitän osaamistani verkkosivujen ja digitaalisten tuotteiden suunnittelun toteuttamisen parissa.</p>
+            <p>Teen itselleni ja asiakkailleni täysin kustomoituja verkkosivuja käyttäen nykyaikaisia teknologioita.</p>
+            <p>Päivätöikseni toimin <span class="text-bold">Product Ownerina</span> huippuluokan rekrytointiohjelmistoa toimittavassa yrityksessa nimeltä <a class="text-jobilla" href="https://jobilla.com/">Jobilla</a></p>
+          </div>
+        </section>
+        <section id="website-project">
+          <div>
+            <h1 class="text-l">Haluatko verkkosivut?</h1>
+            <p>Toteutan sivutoimisesti verkkosivuja ja voin auttaa digitaalisten palveluiden prototyyppaamisessa. Henkilökohtainen oppimisen ollessa yksi osa palkastani, pystyn toteutaamaan projektisi kohtuu kustannuksin.</p>
+            <button class="btn">Aloitetaan projekti yhdessä!</button>
+          </div>
+          <div>
+            <h3>Projektin kulku</h3>
+            <div class="project-step" >
+              <h4>Ollaan yhteyksissä</h4>
+              <p>Ota minuun yhteyttä ja jutellaan minkälainen projekti sinulla olisi mielessä. Katsotaan yhdessä toteutuksen kokonaisuus ja arvioidaan kustannukset.</p>
+            </div>
+            <div class="project-step">
+              <h4>Suunnittelu</h4>
+              <p>Hahmottelen ensivedoksen projektista Figmalla ja käydään se yhdessä läpi. Katsotaan että kaikki tarvittavat elementit löytyvät ja että sivun asettelu on haluamasi mukainen.</p>
+            </div>
+            <div class="project-step">
+              <h4>Toteutus</h4>
+              <p>Pistetään näppäimistö sauhuamaan! Toteutan ensimmäisen käytettävän prototyypin (MVP) ja testataan toimivuus yhdessä.</p>
+            </div>
+            <div class="project-step">
+              <h4>Viimeistely</h4>
+              <p>Hiotaan lopullinen ulkoasu ja sen yksityiskohdat paikalleen. Tarkastan että ulkoasu ja käytettävyys ovat huippuluokkaa niin mobiililaitteilla kuin pöytäkoneillakin.</p>
+            </div>
+            <div class="project-step">
+              <h4>Julkaisu</h4>
+              <p>Hankitaan viimeistään tässä vaiheessa sinulle palvelintila ja verkkotunnus josta verkkosivusi löytyvät. Autan ja opastan kaikissa vaiheissa.</p>
+            </div>
+            <div class="project-step">
+              <h4>Ylläpito ja seuranta</h4>
+              <p>Takaisinmaksun aika! Nyt verkkosivusi ovat koko maailman löydettävissä ja pidetään huoli että ne myös löytyvät. Halutessasi voimme lisätä projektiin analytiikkaa jonka avulla toimintaa ja sisältöä voidaan kehittää.</p>
+            </div>
+          </div>
+        </section>
+        <!-- TODO - Contact Form here -->
+        <section id="about-me">
+          <div>
+            <h1>Tekijästä</h1>
+            <p>Haluatko tietää vielä lisää?</p>
+            
+          </div>
+        </section>
     </div>
-
+   
+    <footer>
+        <div>
+          <p>Footer text</p>
+        </div>
+      </footer>
   </Layout>
 </template>
 
