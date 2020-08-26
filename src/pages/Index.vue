5435d468b61a2a691948a277b0d46e36b101644c
zigelzi <miika.a.savela@gmail.com>
2020-08-26 20:16:42 +0300

    Add utility classes for padding and margin. Change hero image. Update text styling and general body content styling

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -11,9 +11,9 @@
         </div>
       </div>
     </section>
-    <div class="body-content">
+    <div class="">
       <section id="introduction">
-        <div>
+        <div class="text-content ph-xl">
           <h1 class="text-l">Digitaalisten palveluiden moniosaaja</h1>
           <p>Moi!</p>
           <p>
@@ -39,71 +39,74 @@
         </div>
       </section>
       <section id="website-project">
-        <div>
-          <h1 class="text-l">Haluatko verkkosivut?</h1>
-          <p>
-            Toteutan sivutoimisesti verkkosivuja ja voin auttaa digitaalisten
-            palveluiden prototyyppaamisessa. Henkilökohtainen oppimisen ollessa
-            yksi osa palkastani, pystyn toteutaamaan projektisi kohtuu
-            kustannuksin.
-          </p>
-          <button class="btn">Aloitetaan projekti yhdessä!</button>
-        </div>
-        <div>
-          <h3>Projektin kulku</h3>
-          <div class="project-step">
-            <h4>Ollaan yhteyksissä</h4>
+        <div class="ph-xl bg-white">
+          <div class="text-content">
+            <h1 class="text-l">Haluatko verkkosivut?</h1>
             <p>
-              Ota minuun yhteyttä ja jutellaan minkälainen projekti sinulla
-              olisi mielessä. Katsotaan yhdessä toteutuksen kokonaisuus ja
-              arvioidaan kustannukset.
+              Toteutan sivutoimisesti verkkosivuja ja voin auttaa digitaalisten
+              palveluiden prototyyppaamisessa. Henkilökohtainen oppimisen
+              ollessa yksi osa palkastani, pystyn toteutaamaan projektisi kohtuu
+              kustannuksin.
             </p>
+            <button class="btn">Aloitetaan projekti yhdessä!</button>
           </div>
-          <div class="project-step">
-            <h4>Suunnittelu</h4>
-            <p>
-              Hahmottelen ensivedoksen projektista Figmalla ja käydään se
-              yhdessä läpi. Katsotaan että kaikki tarvittavat elementit löytyvät
-              ja että sivun asettelu on haluamasi mukainen.
-            </p>
-          </div>
-          <div class="project-step">
-            <h4>Toteutus</h4>
-            <p>
-              Pistetään näppäimistö sauhuamaan! Toteutan ensimmäisen käytettävän
-              prototyypin (MVP) ja testataan toimivuus yhdessä.
-            </p>
-          </div>
-          <div class="project-step">
-            <h4>Viimeistely</h4>
-            <p>
-              Hiotaan lopullinen ulkoasu ja sen yksityiskohdat paikalleen.
-              Tarkastan että ulkoasu ja käytettävyys ovat huippuluokkaa niin
-              mobiililaitteilla kuin pöytäkoneillakin.
-            </p>
-          </div>
-          <div class="project-step">
-            <h4>Julkaisu</h4>
-            <p>
-              Hankitaan viimeistään tässä vaiheessa sinulle palvelintila ja
-              verkkotunnus josta verkkosivusi löytyvät. Autan ja opastan
-              kaikissa vaiheissa.
-            </p>
-          </div>
-          <div class="project-step">
-            <h4>Ylläpito ja seuranta</h4>
-            <p>
-              Takaisinmaksun aika! Nyt verkkosivusi ovat koko maailman
-              löydettävissä ja pidetään huoli että ne myös löytyvät. Halutessasi
-              voimme lisätä projektiin analytiikkaa jonka avulla toimintaa ja
-              sisältöä voidaan kehittää.
-            </p>
+          <div class="text-content">
+            <h3 class="text-l mh-l">Projektin kulku</h3>
+            <div class="project-step">
+              <h4>Ollaan yhteyksissä</h4>
+              <p>
+                Ota minuun yhteyttä ja jutellaan minkälainen projekti sinulla
+                olisi mielessä. Katsotaan yhdessä toteutuksen kokonaisuus ja
+                arvioidaan kustannukset.
+              </p>
+            </div>
+            <div class="project-step">
+              <h4>Suunnittelu</h4>
+              <p>
+                Hahmottelen ensivedoksen projektista Figmalla ja käydään se
+                yhdessä läpi. Katsotaan että kaikki tarvittavat elementit
+                löytyvät ja että sivun asettelu on haluamasi mukainen.
+              </p>
+            </div>
+
+            <div class="project-step">
+              <h4>Toteutus</h4>
+              <p>
+                Pistetään näppäimistö sauhuamaan! Toteutan ensimmäisen
+                käytettävän prototyypin (MVP) ja testataan toimivuus yhdessä.
+              </p>
+            </div>
+            <div class="project-step">
+              <h4>Viimeistely</h4>
+              <p>
+                Hiotaan lopullinen ulkoasu ja sen yksityiskohdat paikalleen.
+                Tarkastan että ulkoasu ja käytettävyys ovat huippuluokkaa niin
+                mobiililaitteilla kuin pöytäkoneillakin.
+              </p>
+            </div>
+            <div class="project-step">
+              <h4>Julkaisu</h4>
+              <p>
+                Hankitaan viimeistään tässä vaiheessa sinulle palvelintila ja
+                verkkotunnus josta verkkosivusi löytyvät. Autan ja opastan
+                kaikissa vaiheissa.
+              </p>
+            </div>
+            <div class="project-step">
+              <h4>Ylläpito ja seuranta</h4>
+              <p>
+                Takaisinmaksun aika! Nyt verkkosivusi ovat koko maailman
+                löydettävissä ja pidetään huoli että ne myös löytyvät.
+                Halutessasi voimme lisätä projektiin analytiikkaa jonka avulla
+                toimintaa ja sisältöä voidaan kehittää.
+              </p>
+            </div>
           </div>
         </div>
       </section>
       <!-- TODO - Contact Form here -->
       <section id="interests">
-        <div>
+        <div class="text-content">
           <h1>Kiinnostuksen kohteet</h1>
           <!-- <v-interest-slide /> -->
         </div>
