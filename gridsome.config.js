48188ffface4e562f6f768f9786f0fc95c14c139
zigelzi <miika.a.savela@gmail.com>
2020-08-30 09:19:03 +0300

    Add favicon and SoMe sharing attributes

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Viiru Digital',
-  plugins: []
-}
+  siteName: "Viiru Digital",
+  icon: "./src/viiru_favicon_192x192.png",
+  plugins: [],
+};
--- src/main.js
+++ src/main.js
@@ -6,4 +6,38 @@ import DefaultLayout from '~/layouts/Default.vue';
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout);
+
+  head.meta.push({
+    key: 'og:description',
+    name: 'og:description',
+    content: "Miika Savelan digitaalisten palveluiden ja verkkosivujen koti",
+  });
+
+  head.meta.push({
+    key: 'og:image',
+    name: 'og:image',
+    content: `${process.env.GRIDSOME_BASE_PATH}/viiru_favicon_192x192.png`,
+  });
+
+  head.meta.push({
+    key: 'twitter:description',
+    name: 'twitter:description',
+    content: "Miika Savelan digitaalisten palveluiden ja verkkosivujen koti",
+  });
+
+  head.meta.push({
+    key: 'twitter:image',
+    name: 'twitter:image',
+    content: `${process.env.GRIDSOME_BASE_PATH}/viiru_favicon_192x192.png`,
+  });
+
+  router.beforeEach((to, _from, next) => {
+    // Build URL for every site automatically with Vue-Router
+    head.meta.push({
+      key: "og:url",
+      name: "og:url",
+      content: process.env.GRIDSOME_BASE_PATH + to.path,
+    });
+    next();
+  });
 }
